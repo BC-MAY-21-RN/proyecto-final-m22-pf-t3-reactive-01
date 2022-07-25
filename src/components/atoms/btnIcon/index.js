35cbcc99fc6995ba5d946b7d00b5cfc0c7a10473
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-07-24 18:39:59 -0600

    new functionality to the btnicon component
    This new functionality allows to obtain icons from their respective libraries dynamically

--- src/components/atoms/btnIcon/index.js
+++ src/components/atoms/btnIcon/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import IconIon from 'react-native-vector-icons/Ionicons';
+import IconEvilIcons from 'react-native-vector-icons/EvilIcons';
 import RNBounceable from '@freakycoder/react-native-bounceable';
 
 const Style = StyleSheet.create({
@@ -16,11 +19,9 @@ const Style = StyleSheet.create({
   },
 });
 
-const BtnIcon = ({onPress, styles, ...props}) => {
+const BtnIcon = ({onPress, styles, directory, ...props}) => {
   let CONTAINER_STYLES = [Style.container];
-  if (styles) {
-    CONTAINER_STYLES.push(styles);
-  }
+  styles ? CONTAINER_STYLES.push(styles) : null;
   const waitAnimationBounceable = () => setTimeout(onPress, 50);
 
   return (
@@ -29,9 +30,23 @@ const BtnIcon = ({onPress, styles, ...props}) => {
       onPress={waitAnimationBounceable}
       bounceFriction={2}
       bounceEffect={1.2}>
-      <Icon {...props} />
+      <Icon directory={directory} props={props} />
     </RNBounceable>
   );
+};
+
+const Icon = ({directory, props}) => {
+  //Props name, size, color
+  switch (directory) {
+    case 'Ionicons':
+      return <IconIon {...props} />;
+    case 'EvilIcons':
+      return <IconEvilIcons {...props} />;
+    case 'FontAwesome5':
+      return <IconFontAwesome5 {...props} />;
+    default:
+      return <IconFontAwesome {...props} />;
+  }
 };
 
 export default BtnIcon;
