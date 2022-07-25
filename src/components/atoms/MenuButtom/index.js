958ecd3996dcafc4324dd6bf9f55b89f3584b06d
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-07-24 18:36:04 -0600

    add new file sizes and delete some dependencies
    the purpose of this file is to have a size standard
    In addition, some dependencies that were not used were eliminated.

--- src/components/atoms/MenuButtom/index.js
+++ src/components/atoms/MenuButtom/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Pressable} from 'react-native';
+import {Text, Pressable} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import butonStyles from './butonStyles';
 
