73b21736ab89d2327e84f82bd35c7d6df49d7d2b
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-08-02 00:07:46 -0600

    Created a component for error messages in forms
    Removed duplicate code to make a reusable component

---
+++ src/components/atoms/Form/ErrorMessage/Style.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../../../constants/colors';
+const Style = StyleSheet.create({
+  Container: {
+    flexDirection: 'row',
+  },
+  Error: {
+    color: 'red',
+    fontSize: 13,
+    paddingBottom: 1,
+    marginHorizontal: 6,
+  },
+  IconError: {
+    marginTop: 2,
+    color: 'red',
+    marginLeft: 10,
+  },
+});
+
+export default Style;
