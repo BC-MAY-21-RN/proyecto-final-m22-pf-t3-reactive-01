90a0b78b07ca7bc32b287f3f744cf73a04b52524
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-08-02 00:28:35 -0600

    improved input component code
    the code was refactored to make it better readable and usable

---
+++ src/components/atoms/Form/Input/index.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {AwesomeTextInput} from 'react-native-awesome-text-input';
+import {View, Pressable} from 'react-native';
+import Style from './Style';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import ErrorMessage from '../ErrorMessage';
+import CustomButton from '../CustomButton';
+const Input = ({
+  title,
+  styleTitle,
+  value,
+  styleValue,
+  onChangeText,
+  error,
+  secure,
+  editable = true,
+  styleInput,
+  styleContainer,
+}) => {
+  const [Secure, setSecure] = useState(secure);
+  return (
+    <View style={Style.MainContainer}>
+      <AwesomeTextInput
+        style={[Style.Value, styleValue]}
+        editable={editable}
+        label={title}
+        customStyles={{
+          container: {...Style.Container, ...styleContainer},
+          title: {...Style.Title, ...styleTitle},
+          inputContainer: {...Style.Input, ...styleInput},
+        }}
+        onChangeText={onChangeText}
+        value={value}
+        secureTextEntry={Secure}
+      />
+      {secure && (
+        <CustomButton
+          style={Style.Btn}
+          styleIcon={Style.Icon}
+          onPress={() => {
+            setSecure(!Secure);
+          }}
+          iconName={Secure ? 'eye' : 'eye-slash'}
+        />
+      )}
+      {error && <ErrorMessage errorMsg={error} iconName="exclamation-circle" />}
+    </View>
+  );
+};
+
+export default Input;
