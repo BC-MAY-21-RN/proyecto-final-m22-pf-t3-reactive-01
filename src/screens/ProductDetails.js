1f9c13d873e879f12a38178917b351141a0fc238
Deniz-Navarro <ldeniz1@ucol.mx>
2022-08-03 15:29:38 -0500

    Linking the ProductDetails screen.
    In this commit y I linked the product items with the product details screens to view the likes.

--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -7,33 +7,29 @@ import BtnIcon from '../components/atoms/btnIcon';
 import Header from '../components/atoms/Header';
 import CounterInput from '../components/atoms/CounterInput';
 import CustomButton from '../components/atoms/Form/CustomButton';
+import auth from '@react-native-firebase/auth';
 import {ProductDetailsStyles as Styles} from './Styles';
-const userId = '152346';
-const json = {
-  description:
-    'Computadora PRIDE GAMING CRFT DWN / Designed With NZXT / NVIDIA® GeForce RTX™ 3070 Ti / AMD Ryzen 9 5900X / 32GB RAM / 1TB SSD M.2 NVMe / ENF. LIQ. 280MM / 750W 80+ GOLD ',
-  image:
-    'https://ddtech.mx/assets/uploads/thumb_c25ba6722c213cbf0287d4dd2bc94bf8.png',
-  condition: 'Usado',
-  stock: 4545,
-  price: 154445.56,
-  like: ['123456', '152346'],
-  id: 0,
-  name: 'Monitor Gamer Xzeal XZ4015-1 / Negro / 27" / 1MS / 165HZ / Full HD / VA / G-Sync & Freesync / Frameless / HDMI - DP / 2 años de garantía',
-}; // Eliminar hasta terminar
 
 const ProductDetails = ({route: {params}, navigation}) => {
-  const {like, name, description, image, condition, stock, price} = params.item;
+  const userId = auth().currentUser.uid;
+  const {id, like, name, description, image, condition, stock, price} =
+    params.item;
   const {addItem} = useCart(state => ({addItem: state.addItem}), shallow);
   const [quantity, setquantity] = useState(1);
   const onDecrease = () => (quantity > 1 ? setquantity(quantity - 1) : null);
   const onIncrease = () => setquantity(quantity + 1);
-  const BuyItem = () => addItem({...json, quantity});
-
+  const BuyItem = () => addItem({...params.item, quantity});
+  const [liked, setLiked] = useState();
+  if (like.includes(userId)) {
+    setLiked(true);
+  }
+  const handleLike = () => {
+    setLiked(!liked);
+  };
   return (
     <SafeAreaView style={Styles.MainContainer}>
       <Header
-        name="Producto Detalle"
+        name="Product"
         icon="cart"
         onPress={() => navigation.navigate('Cart')}
         BackBtn
@@ -47,8 +43,8 @@ const ProductDetails = ({route: {params}, navigation}) => {
               iconName={'heart'}
               size={20}
               style={Styles.ImageBtn}
-              styleIcon={like.includes(userId) ? Styles.Red : Styles.Black}
-              onPress={() => {}}
+              styleIcon={liked ? Styles.Red : Styles.Black}
+              onPress={handleLike}
             />
             <BtnIcon
               iconName={'share-social-outline'}
