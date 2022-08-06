8cc187fc59a4b765e2d49ff766369dba2aac5377
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-08-06 17:54:10 -0600

    function was changed for one in Realtime changes
    the function in my products was updated depending on the focus of the screen, this was changed to wait for updates from the server

--- src/screens/MyProducts.js
+++ src/screens/MyProducts.js
@@ -1,30 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
 import VerticalList from '../components/atoms/VerticalList';
 import Header from '../components/atoms/Header';
-import {useIsFocused} from '@react-navigation/native';
-
+import {subscriberMyProducts} from '../auth/cloudFirestore';
+import {useUser} from '../utils/user';
 const MyProducts = ({navigation}) => {
+  const user = useUser(state => state.user);
   const [products, setProducts] = useState([]);
-  const isFocused = useIsFocused();
   useEffect(() => {
-    const current = auth().currentUser.uid;
-    firestore()
-      .collection('Products')
-      .where('uid', '==', current)
-      .get()
-      .then(querySnapshot => {
-        const productsAux = [];
-        querySnapshot.forEach(documentSnapshot => {
-          documentSnapshot.data().documentId = documentSnapshot.id;
-          productsAux.push(documentSnapshot.data());
-        });
-        isFocused && setProducts(productsAux);
-      })
-      .catch(error => console.log(error));
-  }, [isFocused]);
+    const sub = subscriberMyProducts(user.uid, setProducts);
+    return sub;
+  }, [user.uid]);
+
   return (
     <View>
       <Header name="My products" BackBtn />
