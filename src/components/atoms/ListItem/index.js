4b692530f1174245867e02bcb774e59ae809d620
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-08-06 18:00:11 -0600

    removal of url upload function
    the works where the url is downloaded was removed, since the document already has the url from the beginning

--- src/components/atoms/ListItem/index.js
+++ src/components/atoms/ListItem/index.js
@@ -1,57 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {View, Text, Pressable, Image} from 'react-native';
 import ListItemStyles from './ListItemStyles';
-import Icon from 'react-native-vector-icons/Ionicons';
-import storage from '@react-native-firebase/storage';
 
-const ListItem = props => {
-  const {
-    name,
-    price,
-    condition,
-    description,
-    id,
-    navigation,
-    category,
-    stock,
-    userId,
-    image,
-  } = props;
-  const [url, setUrl] = useState(null);
-  const getUri = async path => {
-    const consult = await storage().ref(path).getDownloadURL();
-    try {
-      setUrl(consult);
-    } catch (e) {
-      setUrl(null);
-    }
-  };
-  useEffect(() => {
-    getUri(`Products/${userId}/${image}`);
-  }, [userId, image]);
+const ListItem = ({
+  name,
+  price,
+  condition,
+  description,
+  uid,
+  like,
+  navigation,
+  category,
+  stock,
+  uidUser,
+  image,
+}) => {
   return (
     <Pressable
       style={ListItemStyles.container}
-      /*onPress={() => {
+      onPress={() => {
         navigation.navigate('ProductDetails', {
-          like: null,
-          name: name,
-          description: description,
-          image: url
-            ? url
-            : 'https://www.shutterstock.com/search/not-uploaded-yet',
-          condition: condition,
-          stock: stock,
-          price: price,
+          item: {
+            uid,
+            like,
+            name,
+            description,
+            image,
+            uidUser,
+            condition,
+            stock,
+            price,
+          },
         });
-      }}*/
-    >
+      }}>
       <View>
         <Image
           source={{
-            uri: url
-              ? url
-              : 'https://cdn.shopify.com/s/files/1/0070/7032/files/how-to-price-a-product.jpg?v=1611727768&width=1024',
+            uri: image,
           }}
           style={ListItemStyles.image}
         />
